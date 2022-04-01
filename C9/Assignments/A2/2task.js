3028700fe38c1a56c4e0959e5c8e238d930cf2b6
TabitaM <tabymaha@gmail.com>
2022-04-01 15:08:27 +0200

    Assignment 2 is done

---
+++ C9/Assignments/A2/2task.js
@@ -0,0 +1,10 @@
+export default class Task {
+    constructor(taskName, taskId, state, startHour, endHour){
+        this.taskName = taskName;
+        this.taskId = taskId;
+        this.state = state;
+        this.startHour = startHour;
+        this.endHour = endHour;
+    }
+
+}
