424ba06e45e1fd2e123aa9622f561c174d712d30
BoraElena <mironelenagabriela@yahoo.com>
2022-05-31 20:00:35 +0300

    change UserName.js

---
+++ C16/C16/router-tutorial/src/Fruits.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Fruits() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Fruits
---
+++ C16/C16/router-tutorial/src/UserName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserName() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserName
