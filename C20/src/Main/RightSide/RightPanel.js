2e08efb3cd045c7cafcfcc1c9c925da5800016ec
Alex G <alexg@paymoapp.com>
2022-06-14 21:45:21 +0300

    add resources and reducer code

--- C20/src/Main/RightSide/RightPanel.js
+++ C20/src/Main/RightSide/RightPanel.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import Students from './components/Students';
+import Counter from './components/Counter';
 
 class RightPanel extends Component {
 
@@ -9,6 +10,7 @@ class RightPanel extends Component {
     return (
         <div>
           <Students/>
+          <Counter/>
         </div>
     )
   }
