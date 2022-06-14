30a050a806411ca7be2e171ddec6ec1fb0e76198
Alex G <alexg@paymoapp.com>
2022-06-14 20:43:23 +0300

    add C20 resources

---
+++ C20/src/Main/RightSide/RightPanel.js
@@ -0,0 +1,17 @@
+// App.js
+
+import React, { Component } from 'react';
+import Students from './components/Students';
+
+class RightPanel extends Component {
+
+  render() {
+    return (
+        <div>
+          <Students/>
+        </div>
+    )
+  }
+}
+
+export default RightPanel;
